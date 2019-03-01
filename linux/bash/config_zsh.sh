874384e04fd67dbed2496771e01853f00c00503d
shlinym <jarvisfironman@gmail.com>
2019-03-01 23:54:52 +0800

    fix bug

--- linux/bash/config_zsh.sh
+++ linux/bash/config_zsh.sh
@@ -16,7 +16,7 @@ function linux_install()
     sudo /etc/init.d/ssh start
     
     # install zsh
-    sudo apt install -y zsh
+    sudo apt -y install zsh
     sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 }
 
