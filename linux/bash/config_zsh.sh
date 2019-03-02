e94242f47398f51e4385036594d790ca376c4cf3
shlinym <jarvisfironman@gmail.com>
2019-03-02 21:56:56 +0800

    add fzf tool

--- linux/bash/config_zsh.sh
+++ linux/bash/config_zsh.sh
@@ -18,6 +18,15 @@ function linux_install()
     # install zsh
     sudo apt -y install zsh
     sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+    # fzf
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+
+    cp ./.zshrc ~/.zshrc
+
+    # terminal markdown view
+    sudo pip install mdv
 }
 
 OS=$(uname)
