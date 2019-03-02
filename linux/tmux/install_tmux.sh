96876d14aaa6b5cc1c6727ec14b0a2b1e09dd040
shlinym <jarvisfironman@gmail.com>
2019-03-02 22:16:05 +0800

    update tmux

--- linux/tmux/install_tmux.sh
+++ linux/tmux/install_tmux.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt -y install libevent-dev ncurses-dev automake autoconf pkg-config
-git clone https://github.com/tmux/tmux.git
-cd tmux
-sh autogen.sh
-./configure && make
-sudo make install
-cp .tmux.conf ~/
+function macOS_install()
+{
+    brew install tmux
+}
+
+function linux_install()
+{
+    sudo apt-get update
+    sudo apt -y install libevent-dev ncurses-dev automake autoconf pkg-config
+    git clone https://github.com/tmux/tmux.git
+    cd tmux
+    sh autogen.sh
+    ./configure && make
+    sudo make install
+    cd ..
+    cp .tmux.conf ~/
+    rm -rf tmux
+
+    # install plugins
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    tmux source ~/.tmux.conf
+}
+
+OS=$(uname)
+if [ $OS == "Darwin" ];then
+    macOS_install
+elif [ $OS == "Linux" ];then
+    linux_install
+fi
