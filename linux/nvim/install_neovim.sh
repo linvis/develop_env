521b736c6781785bc6f0e5797ab91820866602b4
shlinym <jarvisfironman@gmail.com>
2019-03-07 08:59:59 +0800

    [nvim] fix bug

--- linux/nvim/install_neovim.sh
+++ linux/nvim/install_neovim.sh
@@ -31,7 +31,7 @@ sudo apt -y install python-pip
 sudo pip install --user --upgrade neovim
 
 sudo apt -y install python3-pip
-pip3 install --user --upgrade neovim
+sudo pip3 install --user --upgrade neovim
 # check_python=`python -c 'import sys; print(sys.version_info[:])'`
 # if [ ${check_python:1:2} == "2," ]
 # then
